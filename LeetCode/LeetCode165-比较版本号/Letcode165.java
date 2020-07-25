5993ba2a17d3b9e7027cbb94859e4e6a7350171b
老黑 <976568399@qq.com>
2020-07-25 23:09:31 +0800

    2020-7-25

---
+++ LeetCode/LeetCode165-比较版本号/Letcode165.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+public class Letcode165 {
+	public int compareVersion(String version1, String version2) {
+		// 思路，从左到右一个一个版本号对比
+		int len1 = version1.length();
+		int len2 = version2.length();
+		int k = 0;
+		int startIndex1 = 0, endIndex1 = 0, startIndex2 = 0, endIndex2 = 0;
+		while (endIndex1 < len1 || endIndex2 < len2) {
+			// 先找V1的最左边的版本号
+			int num1 = 0;
+			for (; endIndex1 < len1; endIndex1++) {
+				if (version1.charAt(endIndex1) == '.')
+					break;
+			}
+			if (endIndex1 != startIndex1) { // 如果开始和结束的位置不相等，说明当前可以截取字段，如果相等，就默认用0替代，用于后面的比较
+				num1 = Integer.valueOf(version1.substring(startIndex1, endIndex1));
+			}
+			// 再找V2的最左边的版本号
+			int num2 = 0;
+			for (; endIndex2 < len2; endIndex2++) {
+				if (version2.charAt(endIndex2) == '.')
+					break;
+			}
+			if (endIndex2 != startIndex2) {
+				num2 = Integer.valueOf(version2.substring(startIndex2, endIndex2));
+			}
+			
+			if (num1 > num2)
+				return 1;
+			if (num1 < num2)
+				return -1;
+			startIndex1 = ++endIndex1;
+			startIndex2 = ++endIndex2;
+		}
+		return 0;
+	}
+}
