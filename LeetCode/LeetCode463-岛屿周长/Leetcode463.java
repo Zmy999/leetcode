b15493b1c5466eaf5f1c5fd67efe39697d7601eb
老黑 <976568399@qq.com>
2020-08-18 10:33:07 +0800

    2020-8-18

---
+++ LeetCode/LeetCode463-岛屿周长/Leetcode463.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class Leetcode463 {
+	public int islandPerimeter(int[][] grid) {
+		for (int r = 0; r < grid.length; r++) {
+			for (int c = 0; c < grid[0].length; c++) {
+				if (grid[r][c] == 1) {
+					// 题目限制只有一个岛屿，计算一个即可
+					return dfs(grid, r, c);
+				}
+			}
+		}
+		return 0;
+	}
+
+	int dfs(int[][] grid, int r, int c) {
+		// 从一个岛屿方格走向网格边界，周长加 1
+		if (!(0 <= r && r < grid.length && 0 <= c && c < grid[0].length)) {
+			return 1;
+		}
+		 // 从一个岛屿方格走向水域方格，周长加 1
+		if (grid[r][c] == 0) {
+			return 1;
+		}
+		// 已遍历过（值为2）的岛屿在这里会直接返回，不会重复遍历
+		if (grid[r][c] != 1) {
+			return 0;
+		}
+		// 将方格标记为已遍历
+		grid[r][c] = 2;
+		return dfs(grid, r - 1, c) + dfs(grid, r + 1, c) + dfs(grid, r, c - 1) + dfs(grid, r, c + 1);
+	}
+}
